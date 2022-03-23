b2f1b8c4403d428ab59fb62c0df5f69812e34afb
Aswinradhamadhavam <aswinradhamadhavam@gmial.com>
2022-03-23 12:51:36 +0530

    changepwd

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,10 @@ from . import views
 urlpatterns=[
     path('',views.index),
     path('main',views.index1),
-    path('main1/<int:id>',views.index2)
+    path('main1/<int:id>',views.index2),
+    path('log',views.login,name="log_user"),
+    path('home',views.home,name="home"),
+    path('logout',views.logout,name="logout_user"),
+    path('profile',views.prof,name="profile"),
+    path('chpwd',views.chpwd,name="chpwd")
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from .models import signup
 # Create your views here.
 def index(request):
@@ -27,8 +28,108 @@ def fb(request):
     return render(request,'facebook.html')
 
 def index2(request,id):
-    
+    if request.method=='POST':
+        name=request.POST['nme']
+        id=request.POST['id']
+        adr=request.POST['adr']
+        phno=request.POST['phn']
+        emil=request.POST['eml']
+        usn=request.POST['usrn']
+        psw=request.POST['pwd']
+        # obj=signup(name=name,address=adr,phno=phno,email=emil,uname=usn,pwd=psw)
+        # obj.save()
+        obj=signup.objects.get(sid=id)
+        obj.name=name
+        obj.adrs=adr
+        obj.pno=phno
+        obj.email=emil
+        obj.usrid=usn
+        obj.pwd=psw
+        obj.save()
+
     
    
     obj=signup.objects.get(sid=id)
     return render(request,'index2.html',{'user':obj,})
+
+
+
+def login(request):
+    msg=""
+    if request.method=='POST':
+        # print('hhhh')
+        usrname=request.POST['username']
+        passwrd=request.POST['pwd']
+        # obj_exist=signup.objects.filter(uname=usrname,pwd=passwrd).exists()
+        # if obj_exist:
+        #     return redirect("home")
+        # else:
+        #     msg="Username or password incorrect"
+        try:
+            obj_exist=signup.objects.get(uname=usrname,pwd=passwrd)
+            request.session['user']=obj_exist.sid
+            return redirect("home")
+        
+        except signup.DoesNotExist:
+            msg="Username or password incorrect"
+
+    return render(request,"login.html",{'msg':msg,})
+
+def home(request):
+    obj=signup.objects.get(sid=request.session['user'])
+    return render(request,"home.html",{'user':obj,})
+
+
+def logout(request):
+    del  request.session['user']
+    request.session.flush()
+    
+    return redirect("log_user")
+
+def prof(request):
+    obj=signup.objects.get(sid=request.session['user'])
+    print(obj)
+    if request.method=='POST':
+        # id=request.session['id']
+        name=request.POST['nme']
+        adr=request.POST['adr']
+        phno=request.POST['phn']
+        emil=request.POST['eml']
+        usn=request.POST['usrn']
+        psw=request.POST['pwd']
+        # obj=signup(name=name,address=adr,phno=phno,email=emil,uname=usn,pwd=psw)
+        # obj.save()
+        obj.name=name
+        obj.adrs=adr
+        obj.pno=phno
+        obj.email=emil
+        obj.usrid=usn
+        obj.pwd=psw
+        obj.save()
+        return redirect("home")
+        
+    return render(request,"profile.html",{'user':obj})
+
+
+def chpwd(request):
+    msg=""
+    if request.method=='POST':
+     oldpwd=request.POST['chpsd']
+     newpwd=request.POST['npsd']
+     conpwd=request.POST['cpsd']
+     obj=signup.objects.get(sid=request.session['user'])
+     curpwd=obj.pwd
+     if curpwd==oldpwd:
+         if newpwd==conpwd:
+             signup.objects.filter(sid=request.session['user']).update(pwd=newpwd)
+             msg="password changed"
+         else:
+             msg="password does not match"
+     else:
+         msg="old password does not match"
+
+
+
+
+
+    return render(request,"changepwd.html",{'msg':msg} )
